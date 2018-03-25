e6548bb5cd1d993f244d1d7e0c2787a19f4c9727
Delton Ding <dsh0416@gmail.com>
2018-03-25 23:36:41 +0800

    Minor update

--- ruby/heap.rb
+++ ruby/heap.rb
@@ -1,16 +1,16 @@
-N = 10000000.freeze
-$h = Array.new
+N = 10000000
+@h = Array.new
 
 def pushDown(pos, n)
   while 2*pos + 1 < n
     j = 2*pos + 1
-    if (j+1 < n) and ($h[j+1] > $h[j])
+    if (j+1 < n) and (@h[j+1] > @h[j])
       j += 1
     end
 
-    break unless $h[pos] < $h[j]
+    break unless @h[pos] < @h[j]
 
-    $h[pos], $h[j] = $h[j], $h[pos]
+    @h[pos], @h[j] = @h[j], @h[pos]
     pos = j
   end
 end
@@ -19,7 +19,7 @@ def main
   start = Time.now
 
   N.times do |i|
-    $h << i
+    @h << i
   end
 
   (N/2).downto(0) do |i|
@@ -28,18 +28,16 @@ def main
 
   n = N
   while n > 1
-    $h[0], $h[n-1] = $h[n-1], $h[0]
+    @h[0], @h[n-1] = @h[n-1], @h[0]
     n -= 1
     pushDown 0, n
   end
 
   N.times do |i|
-    raise "h[i] != i" unless $h[i] == i
+    raise "h[i] != i" unless @h[i] == i
   end
 
   puts "Done in #{((Time.now - start) * 1000).to_i}"
 end
 
-begin
-  main
-end
+main
